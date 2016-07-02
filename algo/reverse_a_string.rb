d435bb07fa75c173ce88acdcac92358a0dcdf6ed
Ramiro Trejo <trejoramiro1@gmail.com>
2016-07-02 12:14:55 -0500

    Added solution for reverse string problem.

--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -4,6 +4,18 @@
 
 def reverse_a_string(string)
 
+  reverse_string = ""
+
+  index = 0
+
+  string.length.times do 
+
+    index = index - 1
+    reverse_string = reverse_string + string[index] 
+  end 
+
+  return reverse_string
+  
 end
 
 # Driver code - don't touch anything below this line.
